710d409a6b70f4735cabae4ddb09bfb955c7812a
Odii Daniel <xenithheight@gmail.com>
2025-02-10 19:06:34 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <!-- {{ products }} <span>A BREAAAAAK</span> -->
 
       <div v-for="product in products.products" class="bg-slate-200 mb-20">
-        <<NuxtLink :to="`/products/${product._id}`"></NuxtLink>>{{ product.name }} <br/> {{ product._id }}</NuxtLi><br/>
+        <NuxtLink :to="`/products/${product._id}`">{{ product.name }} <br/> {{ product._id }}</NuxtLink>
       </div>
 
       
