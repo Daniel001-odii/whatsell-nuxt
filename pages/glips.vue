c609b43c4e4fb7b824fd6482422d63a619b03d3a
Odii Daniel <xenithheight@gmail.com>
2025-02-25 00:21:57 +0100

    update glips now working GROK DID IT

--- pages/glips.vue
+++ pages/glips.vue
@@ -1,34 +1,45 @@
 <template>
+   <!-- TEST COMPONENT STARTS HERE -->
+<!-- GROK - WORKS!   -->
+<div class="reels-container">
     <GlipComponent
-    :stories="stories"
+      v-for="(video, index) in videos"
+      :key="index"
+      :video-src="video"
     />
-    <div>
+  </div>
+
+  <!-- <GlipComponent :videos="videos"/> -->
+<!-- TEST COMPONENT ENDS HERE -->
+
+    <!-- <div> -->
         <h1>From Shops youre following</h1>
         <div class=" flex overflow-x-auto p-3 gap-3">
 
             <!-- GLIP CARD -->
-             <div class="min-w-[130px]  w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 bg-opacity-20">
+           <!--   <div class="min-w-[130px]  w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 bg-opacity-20">
                 <div class=" size-14 rounded-full bg-green-500 flex justify-center items-center">
                     <i class="bi bi-plus"></i>
                 </div>
              </div>
              <div v-for="item in products.length" class=" min-w-[130px] w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 bg-opacity-20">
                 <div class=" size-14 rounded-full bg-green-500"></div>
-             </div>
+             </div> -->
         </div>
 
         <h2 class=" font-bold text-lg mt-12">Best deals for you today</h2>
         <div class=" flex flex-wrap mt-4 gap-1">
 
-            <div v-for="item in products" class=" flex flex-col rounded-md p-3 justify-end items-end  h-[200px] bg-gray-500 bg-opacity-20 w-fit">
+          <!--   <div v-for="item in products" class=" flex flex-col rounded-md p-3 justify-end items-end  h-[200px] bg-gray-500 bg-opacity-20 w-fit">
                 <div class=" flex flex-col">
                     <span>Product name</span>
                     <span>NGN Product Price</span>
                     <small>location...</small>
                 </div>
-            </div>
+            </div> -->
         </div>
-    </div>
+    <!-- </div> -->
+<!-- </div> -->
    <!--  <div class=" flex flex-wrap gap-3">
         <GlipCard class=" w-[200px] flex-1"
         v-for="product in products"
@@ -74,9 +85,15 @@ const getProducts = async()=>{
 }
 getProducts();
 
-const stories = [
-  { url: 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9' },
-  { url: 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9' },
+/* const videos = [
+  { url: 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9'},
+  { url: 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9'},
+]; */
+const videos = [
+   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
 ];
 // https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/product-glips/saveinsta.cc_720p-short-ankara-dresses-for-every-occasion-ankarastyles2023-africanclothing-dress-youtubeshorts.mp4
 /* const { data: products, error: products_error } = await useAsyncData('products', async () => {
@@ -86,5 +103,13 @@ const stories = [
 </script>
 
 <style scoped>
+.reels-container {
+  height: 100vh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+  position: fixed;
+  z-index: 999999;
+  top: 0px;
+}
 
 </style>
