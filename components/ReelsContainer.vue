4edca1766508d796df1e505b888e03829c363cf0
Odii Daniel <xenithheight@gmail.com>
2025-02-25 21:17:05 +0100

    update

--- components/ReelsContainer.vue
+++ components/ReelsContainer.vue
@@ -1,48 +1,95 @@
 <template>
-<div class=" bg-white dark:bg-gray-800  h-[90vh] fixed z-[9] top-[0] md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2 w-full md:w-[90vw] md:p-5 flex justify-center items-end md:items-center border rounded-lg dark:border-gray-600">
+  <div v-if="isOpen" @click.self="close"
+    class=" bg-white dark:bg-gray-800  h-[93vh] fixed z-[9] top-[0] md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2 w-full md:w-[90vw] md:p-5 flex justify-center items-end md:items-center border rounded-lg dark:border-gray-600">
+    <UButton color="green" class="absolute top-5 left-5 z-10 rounded-full" icon="material-symbols:chevron-left" variant="soft"
+      @click="close" />
+
+    <!-- MUTE AUDIO BUTTON -->
+    <!-- <div class="absolute top-5 left-5 p-5 z-10">
+      <button @click="close" class=" bg-black bg-opacity-50 rounded-full p-4 justify-center items-center">
+        <i class="bi bi-chevron-left"></i>
+      </button>
+    </div>
+ -->
     <div class="md:w-fit w-full overflow-y-auto h-[100%] inner">
-        
-    <GlipComponent
-      v-for="(video, index) in videos"
-      :key="index"
-      :video-src="video"
-    />
- </div>
-</div>
-<!--  <div class="reels-container">
-    <GlipComponent
-      v-for="(video, index) in videos"
-      :key="index"
-      :video-src="video"
-    />
- </div> -->
+      <!-- {{ glips }} -->
+      <GlipComponent v-for="(item, index) in glips.glips" :key="index" 
+      :video-src="item.video_url" 
+      :glip="item" 
+      :shop="glips.shop"
+      />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
+
+/* export default {
     props: {
-        videos: {
+        glips: {
             required: true,
         },
+        isOpen: {
+          type: Boolean,
+          required: true,
+        },
     },
-}
+  }
+    setup(){ */
+
+
+// Props to control the modal
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+  glips: {
+    required: true,
+  },
+});
+
+const emit = defineEmits(['close']);
+
+// Close modal function
+const close = () => {
+  emit('close');
+};
+
+// Optional: Close on ESC key
+const handleEscape = (event) => {
+  if (event.key === 'Escape' && props.isOpen) {
+    close();
+  }
+};
+
+// Add and clean up event listener
+onMounted(() => document.addEventListener('keydown', handleEscape));
+onUnmounted(() => document.removeEventListener('keydown', handleEscape));
+// }
 </script>
 
 <style scoped>
-.reels-container{
+/* .reels-container{
     @apply h-[93vh] overflow-y-auto fixed z-[9] top-[0] md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2 w-full md:w-fit
-}
-.inner{
-    scroll-snap-type: y mandatory;
+} */
+.inner {
+  scroll-snap-type: y mandatory;
 }
 
-.inner{
-  overflow: auto; /* or scroll */
-  scrollbar-width: none; /* For Firefox */
-  -ms-overflow-style: none; /* For Internet Explorer and Edge */
+.inner {
+  overflow: auto;
+  /* or scroll */
+  scrollbar-width: none;
+  /* For Firefox */
+  -ms-overflow-style: none;
+  /* For Internet Explorer and Edge */
 }
 
 .inner::-webkit-scrollbar {
-  display: none; /* For Chrome, Safari, and newer Edge */
+  display: none;
+  /* For Chrome, Safari, and newer Edge */
 }
 </style>
--- pages/glips.vue
+++ pages/glips.vue
@@ -1,41 +1,52 @@
 <template>
-<div>
-    <ReelsContainer
-    :videos="videos"
-    />
-</div>
+    <!-- <div v-if="glip_preview_modal" class=" relative"> -->
+    <div class=" relative">
+
+        <ReelsContainer :glips="currrent_glip" :is-open="glip_preview_modal" @close="glip_preview_modal = false" />
+    </div>
 
     <!-- <div> -->
-        <h1>From Shops youre following</h1>
-        <div class=" flex overflow-x-auto p-3 gap-3">
+    <h1>From Shops youre following</h1>
+    <div class=" flex overflow-x-auto py-3 gap-3">
 
-            <!-- GLIP CARD -->
-           <!--   <div class="min-w-[130px]  w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 bg-opacity-20">
-                <div class=" size-14 rounded-full bg-green-500 flex justify-center items-center">
-                    <i class="bi bi-plus"></i>
-                </div>
-             </div>
-             <div v-for="item in products.length" class=" min-w-[130px] w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 bg-opacity-20">
-                <div class=" size-14 rounded-full bg-green-500"></div>
-             </div> -->
+        <!-- GLIP CARDS -->
+        <!-- CREATE NEW GLIP -->
+        <div
+            class="min-w-[130px]  w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 bg-opacity-20">
+            <div class=" size-14 rounded-full bg-green-500 flex justify-center items-center">
+                <i class="bi bi-plus"></i>
+            </div>
         </div>
 
-        <h2 class=" font-bold text-lg mt-12">Best deals for you today</h2>
-        <div class=" flex flex-wrap mt-4 gap-1">
 
-          <!--   <div v-for="item in products" class=" flex flex-col rounded-md p-3 justify-end items-end  h-[200px] bg-gray-500 bg-opacity-20 w-fit">
-                <div class=" flex flex-col">
-                    <span>Product name</span>
-                    <span>NGN Product Price</span>
-                    <small>location...</small>
-                </div>
-            </div> -->
+        <!-- GLIPS LIST -->
+        <div v-for="item in grouped_glips" @click="openGlipPreview(item)"
+            :Style="`background-image: url(${item?.shop?.profile?.image_url})`"
+            class=" bg-center bg-no-repeat bg-cover overflow-hidden relative min-w-[130px] w-[130px] h-[200px] flex justify-center items-center rounded-lg bg-gray-500 ">
+            <div class=" h-full w-full bg-black bg-opacity-50"></div>
+            <div class=" size-14 rounded-full bg-green-500 overflow-hidden absolute border-4 border-app_green">
+                <img :src="item?.shop?.profile?.image_url" />
+            </div>
+            <div class=" absolute bottom-5 text-sm">{{ item?.shop?.name }}</div>
         </div>
+    </div>
+
+    <h2 class=" font-bold text-lg mt-12">Best deals for you today</h2>
+    <div class=" flex flex-wrap mt-4 gap-1">
+        <div v-for="item in glips"
+            class=" flex flex-col rounded-md p-3 justify-end items-end  h-[200px] bg-gray-500 bg-opacity-20 w-fit">
+            <div class=" flex flex-col">
+                <span>{{ }}</span>
+                <span>NGN Product Price</span>
+                <small>location...</small>
+            </div>
+        </div>
+    </div>
     <!-- </div> -->
-<!-- </div> -->
-   <!--  <div class=" flex flex-wrap gap-3">
+    <!-- </div> -->
+    <!--  <div class=" flex flex-wrap gap-3">
         <GlipCard class=" w-[200px] flex-1"
-        v-for="product in products"
+        v-for="product in glips"
         :has-liked-button="false"
         :product_slug="product.slug"
         />
@@ -43,15 +54,16 @@
 </template>
 
 <script setup>
+const currrent_glip = ref(null);
 const user = ref(null);
 const credits = ref(0);
-const liked_products = ref([]);
-const getUserDetails = async()=> {
+const liked_glips = ref([]);
+const getUserDetails = async () => {
     try {
         const res = await useNuxtApp().$apiFetch(`/user`);
         user.value = res.user;
         credits.value = res.credits;
-        liked_products.value = res.user.liked_products;
+        liked_glips.value = res.user.liked_glips;
         console.log('user: ', res)
     } catch (error) {
         console.log(error);
@@ -59,46 +71,60 @@ const getUserDetails = async()=> {
 }
 
 const checkLikes = (product_id) => {
-    if(liked_products.value.includes(product_id)){
+    if (liked_glips.value.includes(product_id)) {
         return true;
     } else {
         return false;
     }
 };
 
-const products = ref([])
-const getProducts = async()=>{
-    try{
-        const res = await useNuxtApp().$apiFetch('/products');
-        products.value = res.products; 
-        console.log('got products: ', res)
-    }catch(err){
+const glips = ref([])
+const getglips = async () => {
+    try {
+        const res = await useNuxtApp().$apiFetch('/shops/glips/all');
+        glips.value = res.glips;
+        console.log('got glips: ', res)
+    } catch (err) {
         console.log('err getting glisp: ', err);
     }
 }
-getProducts();
+getglips();
+
+const grouped_glips = ref([]);
+const getGroupedGlips = async () => {
+    try {
+        const res = await useNuxtApp().$apiFetch('/products/glips/all_shops');
+        grouped_glips.value = res.data;
+        console.log('grouped glips: ', res.data)
+    } catch (err) {
+        console.log('err getting grouped glisp: ', err);
+    }
+}
+getGroupedGlips();
+
+const glip_preview_modal = ref(false);
+const openGlipPreview = (item) => {
+    currrent_glip.value = item;
+    console.log("current glip: ", item)
+    glip_preview_modal.value = true;
+
+}
+// http://localhost:8000/api/products/glips/all_shops
 
 /* const videos = [
   { url: 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9'},
   { url: 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9'},
 ]; */
 const videos = [
-   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
-   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
-   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
-   'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+    'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+    /* 'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+    'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9',
+    'https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/o/product-glips%2Fsaveinsta.cc_720p-hotankara-short-gown-styles-to-watch-the-full-video-you-can-check-the-description-below.mp4?alt=media&token=ae756fc2-2d83-4b5c-962b-371d0df187c9', */
 ];
-// https://firebasestorage.googleapis.com/v0/b/test-for-mongo.appspot.com/product-glips/saveinsta.cc_720p-short-ankara-dresses-for-every-occasion-ankarastyles2023-africanclothing-dress-youtubeshorts.mp4
-/* const { data: products, error: products_error } = await useAsyncData('products', async () => {
-  const response = await $fetch(`${config.public.apiBase}/products`)
-  return response.products // Extracting the nested products array
-}) */
 
 const glips_modal = ref(true);
 </script>
 
 <style scoped>
-
 /* @apply h-[95vh] overflow-y-auto fixed z-[9] top-[0] left-1/2 -translate-x-1/2  w-full md:w-fit */
-
 </style>
